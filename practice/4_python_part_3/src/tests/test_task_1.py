dee086c6113e44bca8f04a2fcc3a4d6c1d1d959c
Karen Bocardo <karenbocardo@Karens-MBP.griddynamics.com>
2022-12-16 15:02:58 -0600

    first task of part 3: calculate days to now given custom date

--- practice/4_python_part_3/src/tests/test_task_1.py
+++ practice/4_python_part_3/src/tests/test_task_1.py
@@ -1,1 +1,23 @@
 from src import task_1
+
+# test exception WrongFormatException calculate_days('10-07-2021')
+# calculate_days('2021-10-07')
+# calculate_days('2021-10-05'
+import pytest
+from datetime import datetime
+from freezegun import freeze_time
+
+@freeze_time("2022-12-16")
+def test_correct_format():
+    days = task_1.calculate_days("2022-12-01") # 15 days ago
+    assert days == 15
+
+@freeze_time("2022-12-16")
+def test_negative():
+    days = task_1.calculate_days("2023-12-16") # next year
+    assert days == -365
+
+@freeze_time("2022-12-16")
+def test_incorrect_format():
+    with pytest.raises(task_1.WrongFormatException) as e:
+        days = task_1.calculate_days("12-01-2022")
