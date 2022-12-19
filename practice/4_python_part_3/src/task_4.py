be095834af6f45b4f1ccedd02b1ab9211af2655b
Karen Bocardo <karenbocardo@Karens-MBP.localdomain>
2022-12-19 11:41:47 -0600

    task 4 done: argparse and faker

--- practice/4_python_part_3/src/task_4.py
+++ practice/4_python_part_3/src/task_4.py
@@ -14,12 +14,40 @@ Example:
 {"some_name": "Courtney Duncan", "fake-address": "8107 Nicole Orchard Suite 762\nJosephchester, WI 05981"}
 """
 
-import argparse
+# !!! https://stackoverflow.com/questions/37367331/is-it-possible-to-use-argparse-to-capture-an-arbitrary-set-of-optional-arguments
+# https://www.digitalocean.com/community/tutorials/how-to-use-argparse-to-write-command-line-programs-in-python
+# https://learnpython.com/blog/argparse-module/
+# https://stackoverflow.com/questions/27181084/how-to-iterate-over-arguments
 
+import argparse
+import sys
+from faker import Faker
+
+faker = Faker()
 
 def print_name_address(args: argparse.Namespace) -> None:
-    ...
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num', type=int)
+    args, unknown = parser.parse_known_args()
+    
+    # add fields to parser
+    for arg in unknown:
+        if arg.startswith(("-", "--")):
+            parser.add_argument(arg.split('=')[0])
+    args = parser.parse_args()
 
+    # remove number from args
+    number = args.num
+    del args.num
+
+    dict_ls = list()
+    for i in range(number):
+        new_dict = dict()
+        for field, provider in vars(args).items():
+            new_dict[field] = eval(f"faker.{provider}()")
+        print(new_dict)
+        dict_ls.append(new_dict)
+    
 
 """
 Write test for print_name_address function
@@ -30,3 +58,5 @@ Example:
     >>> m.method()
     123
 """
+
+print_name_address(sys.argv)
