80d1a0856a43c89c5ae4d782a9943f021ed612be
Karen Bocardo <karenbocardo@Karens-MBP.griddynamics.com>
2023-01-09 10:32:45 -0600

    setup tests

---
+++ capstone/tests/test_utility.py
@@ -0,0 +1,51 @@
+from src.utility import ConsoleUtility
+import pytest
+
+def test_data_types():
+    '''
+    Write a parameterized test for different data types.
+    '''
+    pass
+
+def test_data_schemas():
+    '''
+    Write a parameterized test for different data schemas.
+    '''
+    pass
+
+def test_load_json():
+    '''
+    - Write a test that uses temporary files to test your program when the data schema is loaded with a json file. 
+    You have to use fixtures here.
+    '''
+    pass
+
+def test_clear_path():
+    '''
+    Test for the “clear_path” action.
+    '''
+    pass
+
+def test_save_file():
+    '''
+    Test to check saving file to the disk.
+    '''
+    pass
+
+def test_multiprocess():
+    '''
+    Write a test to check a number of created files if “multiprocessing” > 1.
+    '''
+    pass
+
+def test_custom():
+    '''
+    Write your own test.
+    '''
+    pass
+
+'''
+useful links:
+
+- https://jugmac00.github.io/blog/testing-argparse-applications-the-better-way/
+'''
